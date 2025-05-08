461cffdb6353b0e3839dbdc18c90abe95ac70045
lep7405 <lep7402@gmail.com>
2025-05-08 14:32:35 +0700

    fix index discounts

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -27,45 +27,58 @@ class DiscountController extends Controller
         Log::debug('✅ Laravel Cloud log test');
         $perPage = Arr::get($request->all(), 'perPageDiscount');
         $search = Arr::get($request->all(), 'searchDiscount');
-        $startedAt = Arr::get($request->all(), 'startedAt');
+        $sortStartedAt = Arr::get($request->all(), 'sortStartedAt');
         $pageDiscount = Arr::get($request->all(), 'pageDiscount');
-        $query = Discount::query();
-        $query = $query->when($search, function ($query) use ($search) {
-            $query
-                ->where(function ($sub) use ($search) {
-                    if ($search) {
-                        $escapedSearch = addcslashes($search, '%_');
-                        $sub->where('name', 'like', "%{$escapedSearch}%");
-                        $sub->orWhere('started_at', 'like', "%{$escapedSearch}%");
-                        $sub->orWhere('expired_at', 'like', "%{$escapedSearch}%");
-                    }
-                })
-                ->orWhere(function ($sub) use ($search) {
-                    if (is_numeric($search)) {
-                        $sub->where('id', $search);
-                    }
-                })
-                ->orWhere(function ($sub) use ($search) {
-                    if ($search) {
-                        $cleanSearch = str_replace('%', '', $search);
-                        $sub->where('value', $cleanSearch);
-                    }
-                })
-            ;
-        });
-        $query = $query->when($startedAt, function ($query) use ($startedAt) {
-            $query->orderBy('started_at', $startedAt);
-        });
+        $withCoupon = Arr::get($request->all(), 'withCoupon', false);
 
-        // Thêm orderBy
+        $query = Discount::query();
+
+        if ($withCoupon) {
+            $query = $query->with(['coupon' => function ($query) {
+                $query->select('id', 'times_used', 'discount_id');
+            }]);
+        }
+
+        if ($search) {
+            $query = $query->where(function ($sub) use ($search) {
+                $escapedSearch = addcslashes($search, '%_');
+                $sub->where('name', 'like', "%{$escapedSearch}%")
+                    ->orWhere('started_at', 'like', "%{$escapedSearch}%")
+                    ->orWhere('expired_at', 'like', "%{$escapedSearch}%");
+
+                if (is_numeric($search)) {
+                    $sub->orWhere('id', $search);
+                }
+
+                $sub->orWhere(function ($q) use ($search) {
+                    $q->where(function ($inner) use ($search) {
+                        $inner->where('type', 'percentage')
+                            ->where('value', str_replace('%', '', $search));
+                    })
+                        ->orWhere(function ($inner) use ($search) {
+                            $inner->where('type', 'amount')
+                                ->where('value', $search);
+                        });
+                });
+            });
+        }
+
+        if ($sortStartedAt && in_array($sortStartedAt, ['asc', 'desc'])) {
+            $query = $query->orderBy('started_at', $sortStartedAt);
+        }
+
         $query = $query->orderBy('id', 'desc');
 
-        // Kết thúc với paginate
-        $query = $query->paginate($perPage, ['*'], 'pageDiscount', $pageDiscount);
-        Log::info('query', ['query' => $query]);
+        $result = $query->paginate(
+            $perPage,
+            ['id','name','started_at','expired_at','type','value','usage_limit','trial_days'],
+            'pageDiscount',
+            $pageDiscount
+        );
+
         return response()->json([
             'message' => 'Discounts retrieved successfully',
-            'discounts' => $query,
+            'discounts' => $result,
         ]);
     }
 }
