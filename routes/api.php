35b61dc2323a19b3cbd3e616c6fd5b00d17c2292
lep7405 <lep7402@gmail.com>
2025-05-15 16:54:04 +0700

    fix store

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/discount/create',[DiscountController::class, 'store'])->name('discount.create');
 Route::get('/discounts', [DiscountController::class, 'index'])->name('discounts.index');
 Route::get('/discount/{id}', [DiscountController::class, 'findDiscountById'])->name('discounts.findDiscountById');
-Route::get('/discounts/{id}', [DiscountController::class, 'findDiscountsByIds'])->name('discounts.findDiscountsByIds');
+Route::post('/discounts/find-by-ids', [DiscountController::class, 'findDiscountsByIds'])->name('discounts.findDiscountsByIds');
 
 Route::put('/discount/{id}', [DiscountController::class, 'update'])->name('discounts.update');
 Route::delete('/discount/{id}', [DiscountController::class, 'destroy'])->name('discounts.destroy');
