b1e0c5ceec3879ce04a7ef43ed3ed6f201878d11
lep7405 <lep7402@gmail.com>
2025-05-16 11:59:05 +0700

    fix route

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,27 @@ use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/discount/create',[DiscountController::class, 'store'])->name('discount.create');
+Route::post('/discounts',[DiscountController::class, 'store'])->name('discount.create');
 Route::get('/discounts', [DiscountController::class, 'index'])->name('discounts.index');
-Route::get('/discount/{id}', [DiscountController::class, 'findDiscountById'])->name('discounts.findDiscountById');
+Route::get('/discounts/{id}', [DiscountController::class, 'findDiscountById'])->name('discounts.findDiscountById');
 Route::post('/discounts/find-by-ids', [DiscountController::class, 'findDiscountsByIds'])->name('discounts.findDiscountsByIds');
 
 //Route::get('/discounts/all', [DiscountController::class, 'getAllDiscounts'])->name('discounts.index');
 Route::get('/discounts/id-and-name', [DiscountController::class, 'getIdAndName'])->name('discounts.idAndName');
-Route::post('/discount/affiliate-partners',[DiscountController::class, 'UpdateOrCreateDiscountInAffiliatePartner'])->name('discounts.UpdateOrCreateDiscountInAffiliatePartner');
+Route::post('/discounts/affiliate-partners',[DiscountController::class, 'UpdateOrCreateDiscountInAffiliatePartner'])->name('discounts.UpdateOrCreateDiscountInAffiliatePartner');
 
-Route::put('/discount/{id}', [DiscountController::class, 'update'])->name('discounts.update');
-Route::delete('/discount/{id}', [DiscountController::class, 'destroy'])->name('discounts.destroy');
+Route::put('/discounts/{id}', [DiscountController::class, 'update'])->name('discounts.update');
+Route::delete('/discounts/{id}', [DiscountController::class, 'destroy'])->name('discounts.destroy');
 Route::get('/discounts/with-coupons', [DiscountController::class, 'getDiscountsWithCoupons'])->name('discounts.withCoupons');
 
 Route::get('/coupons', [CouponController::class, 'index'])->name('coupons.index');
-Route::post('/coupon',[CouponController::class, 'store'])->name('coupons.create');
-Route::get('/coupon/id/{id}', [CouponController::class, 'findCouponById'])->name('coupons.findCouponById');
-Route::get('/coupon/code/{code}', [CouponController::class, 'findCouponByCode'])->name('coupons.findCouponByCode');
-Route::put('/coupon/{id}', [CouponController::class, 'update'])->name('coupons.update');
-Route::put('/coupon/{id}/status', [CouponController::class, 'updateStatus'])->name('coupons.updateStatus');
-Route::put('/coupon/{id}/times-used', [CouponController::class, 'decrementTimesUsed'])->name('coupons.decrementTimesUsed');
-Route::delete('/coupon/{id}', [CouponController::class, 'destroy'])->name('coupons.destroy');
-Route::get('/coupon/discount/{id}', [CouponController::class, 'findCouponByDiscountIdAndCode'])->name('coupons.findCouponByDiscountIdAndCode');
-Route::get('/coupon/discount/{id}/shop/{shop}/', [CouponController::class, 'findCouponByDiscountIdAndShop'])->name('coupons.findCouponByDiscountIdAndShop');
+Route::post('/coupons',[CouponController::class, 'store'])->name('coupons.create');
+Route::get('/coupons/id/{id}', [CouponController::class, 'findCouponById'])->name('coupons.findCouponById');
+Route::get('/coupons/code/{code}', [CouponController::class, 'findCouponByCode'])->name('coupons.findCouponByCode');
+Route::put('/coupons/{id}', [CouponController::class, 'update'])->name('coupons.update');
+Route::put('/coupons/{id}/status', [CouponController::class, 'updateStatus'])->name('coupons.updateStatus');
+Route::put('/coupons/{id}/times-used', [CouponController::class, 'decrementTimesUsed'])->name('coupons.decrementTimesUsed');
+Route::delete('/coupons/{id}', [CouponController::class, 'destroy'])->name('coupons.destroy');
+Route::get('/coupons/discount/{id}', [CouponController::class, 'findCouponByDiscountIdAndCode'])->name('coupons.findCouponByDiscountIdAndCode');
+Route::get('/coupons/discount/{id}/shop/{shop}/', [CouponController::class, 'findCouponByDiscountIdAndShop'])->name('coupons.findCouponByDiscountIdAndShop');
 Route::get('/reports',[ReportController::class, 'index'])->name('reports.index');
-
-Route::get('/', function () {
-    return view('welcome');
-});
